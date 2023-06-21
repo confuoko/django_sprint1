51bf3d385f37c2d93490e4bd23fa5fe3eae8106d
MatveevaNatali <matveeva.skype@mail.ru>
2023-06-21 20:56:41 +0300

    Готовый проект исправленный.v2

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name='pages'
+app_name = 'pages'
 
-urlpatterns= [
+urlpatterns = [
     path('about/', views.about, name='about'),
     path('rules/', views.rules, name='rules'),
 ]
